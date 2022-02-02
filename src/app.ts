967af728b6240c5d8be40932cdbe32b856d4744a
a-kuz <33007777+a-kuz@users.noreply.github.com>
2022-02-02 10:56:03 +0300

    w

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,12 @@ import "./styles.scss";
 
 // Creating the sketch itself
 const sketch = (p5: P5) => {
-  p5.randomGaussian()
+  p5.randomGaussian();
   let p = p5.PI;
   let r = 300;
   const margin = 40;
   function randomChord() {
-		r=300;
+    r = 300;
     if (p > p5.PI / 4) p -= 0.0009;
     else p = p5.PI;
     // find a random point on a circle
@@ -29,12 +29,12 @@ const sketch = (p5: P5) => {
     p5.stroke(p5.random(0, 70), p5.random(150, 190), p5.random(220, 235), 25);
 
     // draw a line between them
-    p5.strokeWeight(0.2);
+    //p5.strokeWeight(0.2);
 
     p5.line(xpos1, ypos1, xpos2, ypos2);
 
     if (1 === 1) {
-      r = 310;
+      r = 300;
       let angle1 = p5.random(0, 2 * p5.PI);
       let xpos1 = r + r * p5.cos(angle1) + margin / 0.99;
       let ypos1 = r + r * p5.sin(angle1) + margin / 0.99;
@@ -44,12 +44,7 @@ const sketch = (p5: P5) => {
       let xpos2 = r + r * p5.cos(angle2) + margin / 0.99;
       let ypos2 = r + r * p5.sin(angle2) + margin / 0.99;
 
-      p5.stroke(
-        p5.random(170, 70),
-        p5.random(220, 235),
-        p5.random(220, 235),
-        20
-      );
+      p5.stroke(255, 255, 255, 7);
 
       // draw a line between them
       p5.strokeWeight(0.1);
@@ -57,14 +52,26 @@ const sketch = (p5: P5) => {
       p5.line(xpos1, ypos1, xpos2, ypos2);
     }
 
-    if (p5.random() < 10.1) {
+    if (p5.random() < 0.001) {
       let angle1 = p5.random(0, 2 * p5.PI);
-      let rr = p5.random(0, r);
+      let rr = p5.random(r * 1.2, r * 1.7);
       let xpos1 = r + rr * p5.cos(angle1) + margin;
-			let ypos1 = r + rr * p5.sin(angle1) + margin;
-			p5.fill(70,170,255,90)
-			p5.circle(xpos1, ypos1, p5.random(1,20));
-			
+      let ypos1 = r + rr * p5.sin(angle1) + margin;
+      let rrr = p5.randomGaussian(1, 20);
+      p5.fill(0xff, 0xfb, 0xd4, 200);
+      p5.circle(xpos1, ypos1, rrr);
+      p5.fill(0xef, 0xd1, 0x7f, 6);
+
+      p5.circle(xpos1, ypos1, rrr * 1.4);
+      p5.fill(0xef, 0xd1, 0x7f, 4);
+      p5.circle(xpos1, ypos1, rrr * 2);
+      p5.fill(0xef, 0xd1, 0x7f, 2);
+      p5.circle(xpos1, ypos1, rrr * 3);
+      p5.fill(0xef, 0xd1, 0x7f, 2);
+      p5.circle(xpos1, ypos1, rrr * 5);
+      p5.fill(0xef, 0xd1, 0x7f, 4);
+      p5.circle(xpos1, ypos1, rrr * 20);
+      p5.dr;
     }
   }
 
@@ -82,7 +89,21 @@ const sketch = (p5: P5) => {
 
   // The sketch draw method
   p5.draw = () => {
-    // DEMO: Let the circle instances draw themselves
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
+    randomChord();
     randomChord();
     randomChord();
     randomChord();
